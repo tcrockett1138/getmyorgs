96b026bacf69a31e767e11f1d1674059168696b4
Tim Crockett <15042033+tcrockett1138@users.noreply.github.com>
2018-08-03 18:50:50 -0600

    remove non-working and non-useful list spaces (no context of space to org so duplicates are confusing)

--- getMyOrgs.sh
+++ getMyOrgs.sh
@@ -44,27 +44,9 @@ if [[ -z $orgs_url ]]; then
 fi
 
 
-# Get the orgs and spaces for the given user
+# Get the orgs for the given user
 user_orgs=$(cf curl $orgs_url | jq -r '.resources | .[].entity.name')
 
 printf "\nThe user '$user' is a member of the following orgs:\n"
 printf "$user_orgs\n"
 
-# Is the user a member of any spaces?
-spaces_url=$(cf curl /v2/users?results-per-page=100\&page=$user_page | jq -r --arg user "$user" '.resources[].entity | select(.username == $user) | .spaces_url')
-if [[ -z $spaces_url ]]; then
-  echo checking for empty spaces url
-  printf "\nThe user '$user' is not a member of any spaces\n"
-elif (( $(cf curl $spaces_url | jq -r .total_results) == 0 )); then
-  printf "\nThe user '$user' is not a member of any spaces\n\n"
-else
-  space_pages=$(cf curl $spaces_url | jq -r .total_pages)
-  space_page=1
-  echo "Found $space_pages pages of spaces for '$user'"
-  while (( $space_page <= $space_pages )); do
-    user_spaces+=$(cf curl $spaces_url?result-per-page=100\&page=$space_page | jq -r '.resources | .[].entity.name')
-    (( space_page++ ))
-  done
-  printf "\nThe user '$user' is a member of the following spaces:\n"
-  printf "$user_spaces\n\n"
-fi
